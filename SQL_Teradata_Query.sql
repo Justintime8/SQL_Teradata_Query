a384551ffca83156a060383b61b93464b6eb8eae
Justintime8 <49258832+Justintime8@users.noreply.github.com>
2020-11-03 18:47:24 -0500

    Update SQL_Teradata_Query.sql

--- SQL_Teradata_Query.sql
+++ SQL_Teradata_Query.sql
@@ -1,15 +1,14 @@
 Database ua_dillards;
 
-SELECT DISTINCT	t.store,
+SELECT DISTINCT t.store, 
                 s.city,	
-                s.state
+				        s.state
 FROM trnsact t	
-JOIN strinfo s ON	t.store = s.store
-WHERE	t.store	IN (SELECT	days_in_month.store
-                  FROM(SELECT	EXTRACT(YEAR	from saledate) AS sales_year,	
-                              EXTRACT(MONTH	from saledate) AS sales_month,	
-                              store,	
-                              COUNT(DISTINCT saledate) as numdays
-                       FROM trnsact
-                       GROUP BY	sales_year,	sales_month,	store
-                       HAVING	numdays=11)	as	days_in_month)
+JOIN strinfo s ON t.store = s.store
+WHERE t.store IN (SELECT days_in_month.store
+				          FROM(SELECT EXTRACT(YEAR from saledate) AS sales_year,	
+				                      EXTRACT(MONTH from saledate) AS sales_month, store,	
+					                    COUNT (DISTINCT saledate) as	numdays
+					             FROM	trnsact
+					             GROUP BY	sales_year,	sales_month, store
+					             HAVING numdays = 11)	as	days_in_month)
